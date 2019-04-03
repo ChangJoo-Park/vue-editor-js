18b364ef9cb96b0bcfa15439eb74bb5cb64ca488
ChangJoo Park <pcjpcj2@gmail.com>
2019-04-03 22:19:38 +0900

    Update demo for save

--- demo/Demo.vue
+++ demo/Demo.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="demo">
-    <editor />
+    <button @click="trySave">Submit</button>
+    <editor
+      @save="save"
+      save-button-id="save-button"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'demo'
+  name: 'demo',
+  methods: {
+    trySave () {
+      this.$el.querySelector('#save-button').click()
+    },
+    save (response) {
+      console.log(response)
+    }
+  }
 }
 </script>
 
