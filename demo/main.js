ce81c5f0a7d83d6136c3582f4a54051610d29726
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-11-19 22:30:42 +0900

    Update DEMO for Vue.js composition-api

--- demo/main.js
+++ demo/main.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import App from './Demo'
 
-// Use it as Plugin
-// import Editor from '../src'
-// Vue.use(Editor)
+import Editor from '../src'
+Vue.use(Editor)
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  // NOTE: if you need to inject as option, you can set here!
-  // plugin,
   render: h => h(App)
 }).$mount('#app')
