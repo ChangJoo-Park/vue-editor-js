90d7796c7212240bfdf315694c9100efff467b3e
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2020-06-05 22:41:23 +0900

    Update dependencies and Demo

--- demo/Demo.vue
+++ demo/Demo.vue
@@ -1,67 +1,216 @@
 <template>
   <div class="demo">
-    <button @click="save">Save & Log</button>
-    <button @click="load">Load</button>
-    <button @click="loadData">Load Example</button>
     <h1 style="text-align: center;">DEMO</h1>
-    <editor
-      id='vue-editor-js'
-      :autofocus="true"
-      :init-data="initData"
-      :config="config"
-      :minHeight="700"
-      :bgColor="'yellow'"
-      ref="editor"
-      placeholder="test"
-      @save="onSave"
-      @ready="onReady"
-      @change="onChange"
-    />
+    <editor ref="editor" :config="config" :initialized="onInitialized"/>
   </div>
 </template>
 
 <script>
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import CodeTool from '@editorjs/code'
+import Paragraph from '@editorjs/paragraph'
+import Embed from '@editorjs/embed'
+import Table from '@editorjs/table'
+import Checklist from '@editorjs/checklist'
+import Marker from '@editorjs/marker'
+import Warning from '@editorjs/warning'
+import RawTool from '@editorjs/raw'
+import Quote from '@editorjs/quote'
+import InlineCode from '@editorjs/inline-code'
+import Delimiter from '@editorjs/delimiter'
+import SimpleImage from '@editorjs/simple-image'
+
 export default {
   data() {
     return {
-      initData: {},
-      savedData: {},
       config: {
-        image: {
-          endpoints: {
-            byFile: "http://39.105.168.171:8090/image",
-            byUrl: "http://39.105.168.171:8090/image-by-url"
+        tools:{
+          header: {
+            class: Header,
+            config: {
+              placeholder: 'Enter a header',
+              levels: [2, 3, 4],
+              defaultLevel: 3,
+            }
           },
-          field: "image",
-          types: "image/*"
+          list: {
+            class: List,
+            inlineToolbar: true,
+          },
+          code: {
+            class: CodeTool
+          },
+          paragraph: {
+            class: Paragraph,
+          },
+          embed: {
+            class: Embed,
+            config: {
+              services: {
+                youtube: true,
+                coub: true,
+                imgur: true
+              }
+            }
+          },
+          table: {
+            class: Table,
+            inlineToolbar: true,
+            config: {
+              rows: 2,
+              cols: 3,
+            },
+          },
+          checklist: {
+            class: Checklist,
+          },
+          Marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M',
+          },
+          warning: {
+            class: Warning,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+W',
+            config: {
+              titlePlaceholder: 'Title',
+              messagePlaceholder: 'Message',
+            },
+          },
+          raw: RawTool,
+          quote: {
+            class: Quote,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+O',
+            config: {
+              quotePlaceholder: 'Enter a quote',
+              captionPlaceholder: 'Quote\'s author',
+            },
+          },
+          inlineCode: {
+            class: InlineCode,
+            shortcut: 'CMD+SHIFT+M',
+          },
+          delimiter: Delimiter,
+          image: SimpleImage,
         },
-        personality: {
-          endpoint: "http://39.105.168.171:8090/image"
+        onReady: () => {
+          console.log('on ready')
+        },
+        onChange: (args) => {
+          console.log('Now I know that Editor\'s content changed!')
+        },
+        data: {
+            "time": 1591362820044,
+            "blocks": [
+                {
+                    "type" : "header",
+                    "data" : {
+                        "text" : "Editor.js",
+                        "level" : 2
+                    }
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text."
+                    }
+                },
+                {
+                    "type" : "header",
+                    "data" : {
+                        "text" : "Key features",
+                        "level" : 3
+                    }
+                },
+                {
+                    "type" : "list",
+                    "data" : {
+                        "style" : "unordered",
+                        "items" : [
+                            "It is a block-styled editor",
+                            "It returns clean data output in JSON",
+                            "Designed to be extendable and pluggable with a simple API"
+                        ]
+                    }
+                },
+                {
+                    "type" : "header",
+                    "data" : {
+                        "text" : "What does it mean «block-styled editor»",
+                        "level" : 3
+                    }
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor's Core."
+                    }
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "There are dozens of <a href=\"https://github.com/editor-js\">ready-to-use Blocks</a> and the <a href=\"https://editorjs.io/creating-a-block-tool\">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games."
+                    }
+                },
+                {
+                    "type" : "header",
+                    "data" : {
+                        "text" : "What does it mean clean data output",
+                        "level" : 3
+                    }
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below"
+                    }
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "Given data can be used as you want: render with HTML for <code class=\"inline-code\">Web clients</code>, render natively for <code class=\"inline-code\">mobile apps</code>, create markup for <code class=\"inline-code\">Facebook Instant Articles</code> or <code class=\"inline-code\">Google AMP</code>, generate an <code class=\"inline-code\">audio version</code> and so on."
+                    }
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "Clean data is useful to sanitize, validate and process on the backend."
+                    }
+                },
+                {
+                    "type" : "delimiter",
+                    "data" : {}
+                },
+                {
+                    "type" : "paragraph",
+                    "data" : {
+                        "text" : "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
+                    }
+                },
+                {
+                    "type" : "image",
+                    "data" : {
+                        "file" : {
+                            "url" : "https://codex.so/public/app/img/external/codex2x.png"
+                        },
+                        "caption" : "",
+                        "withBorder" : false,
+                        "stretched" : false,
+                        "withBackground" : false
+                    }
+                },
+
+            ],
+            "version" : "2.18.0"
         }
-      }
+      },
     };
   },
   methods: {
-    save() {
-      this.$refs.editor.save();
-    },
-    load() {
-      this.initData = this.savedData;
-    },
-    loadData() {
-      this.initData = JSON.parse(
-        '{"time":1558356864490,"blocks":[{"type":"paragraph","data":{"text":"First text"}},{"type":"paragraph","data":{"text":"Second text"}}],"version":"2.13"}'
-      );
-    },
-    onSave(response) {
-      console.log(JSON.stringify(response));
-      this.savedData = response;
-    },
-    onReady() {
-      console.log("ready");
-    },
-    onChange() {
-      console.log("changed");
+    onInitialized (editor) {
+      console.log(editor)
     }
   }
 };
--- src/Editor.vue
+++ src/Editor.vue
@@ -1,82 +1,44 @@
 <template>
-  <div>
-    <div :style="{minHeight: minHeight+'px',background: bgColor}" :id="props.holderId" />
-    <button :id="`${props.holderId}-button`" @click="save" style="display: none;" />
-  </div>
+  <div :id="holder" />
 </template>
 
 <script>
 import {
-  defineComponent,
   reactive,
   onMounted,
-  watch
+  watch,
+  defineComponent
 } from '@vue/composition-api'
 import EditorJS from '@editorjs/editorjs'
-import {
-  DEFAULT_OBJECT_PROP,
-  DEFAULT_BOOLEAN_PROP,
-  PLUGIN_PROPS_TYPE,
-  PLUGIN_PROPS,
-  DEFAULT_STRING_PROP,
-  useTools
-} from './utils'
 
 export default defineComponent({
   name: 'vue-editor-js',
   props: {
-    editorId: {
+    holder: {
       type: String,
-      default: () => 'vue-editor-js'
+      default: () => 'vue-editor-js',
+      require: true
     },
-    minHeight: {
-      type: Number,
-      default: () => 800
+    config: {
+      type: Object,
+      default: () => ({}),
+      require: true
     },
-    bgColor: {
-      type: String,
-      default: () => 'rgba(255,255,255,.6)'
-    },
-    holderId: {
-      type: String,
-      default: () => 'codex-editor',
-      required: false
-    },
-    placeholder: DEFAULT_STRING_PROP,
-    autofocus: DEFAULT_BOOLEAN_PROP,
-    initData: DEFAULT_OBJECT_PROP,
-    customTools: DEFAULT_OBJECT_PROP,
-    config: DEFAULT_OBJECT_PROP,
-    ...PLUGIN_PROPS.reduce(
-      (a, pluginName) => ({ ...a, [pluginName]: PLUGIN_PROPS_TYPE }),
-      {}
-    )
+    initialized: {
+      type: Function,
+      default: () => {}
+    }
   },
   setup: (props, context) => {
     const state = reactive({ editor: null })
 
     function initEditor(props) {
       destroyEditor()
-
-      const {
-        holderId: holder,
-        autofocus,
-        initData: data,
-        config,
-        placeholder
-      } = props
-      console.log(config)
-      const tools = useTools(props, config)
-
       state.editor = new EditorJS({
-        holder,
-        autofocus,
-        placeholder,
-        data,
-        tools,
-        onReady: () => context.emit('ready'),
-        onChange: () => context.emit('change')
+        holder: 'vue-editor-js',
+        ...props.config
       })
+      props.initialized(state.editor)
     }
 
     function destroyEditor() {
@@ -86,25 +48,9 @@ export default defineComponent({
       }
     }
 
-    function save() {
-      if (!state.editor) {
-        return
-      }
-
-      state.editor
-        .save()
-        .then(result => context.emit('save', result))
-        .catch(err => console.error(err))
-    }
-
     onMounted(_ => initEditor(props))
 
-    watch(
-      _ => props.initData,
-      _ => initEditor
-    )
-
-    return { props, state, save }
+    return { props, state }
   }
 })
 </script>
