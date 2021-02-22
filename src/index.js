0b3201f1f18fdff27376365effa43ba3fa46a1f3
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-02-22 23:56:58 +0900

    Add hook for Vue.js 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import EditorComponent from './Editor.vue'
 
-export default {
-  install(Vue) {
-    Vue.component('Editor', EditorComponent);
-  }
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+
+  Vue.component('Editor', EditorComponent);
+}
+
+export function useEditorJS() {
+  const editorJSPlugin = {
+    install(app) {
+      app.component('Editor', EditorComponent);
+    },
+  };
+
+  return editorJSPlugin;
 }
