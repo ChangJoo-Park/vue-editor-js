38135dfba5931aa8482d8621dd66ef0ac2fe553a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-02-23 00:33:47 +0900

    Release 2.1.5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-import Editor from './Editor.vue';
+import { reactive, defineComponent, onMounted, h } from "vue-demi";
+import EditorJS from "@editorjs/editorjs";
 
-export default Editor
+export const Editor = defineComponent({
+  props: {
+    holder: {
+      type: String,
+      default: () => "vue-editor-js",
+      require: true,
+    },
+    config: {
+      type: Object,
+      default: () => ({}),
+      require: true,
+    },
+    initialized: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  setup: (props) => {
+    const state = reactive({ editor: null });
+
+    function initEditor(props) {
+      destroyEditor();
+      state.editor = new EditorJS({
+        holder: props.holder || "vue-editor-js",
+        ...props.config,
+      });
+      props.initialized(state.editor);
+    }
+
+    function destroyEditor() {
+      if (state.editor) {
+        state.editor.destroy();
+        state.editor = null;
+      }
+    }
+
+    onMounted(() => {
+      console.log('on mounted')
+      initEditor(props)
+    });
+
+    return { props, state };
+  },
+  render(props) {
+    console.log('render')
+    return h(
+      'div',
+      {
+        id: props.holder
+      },
+    )
+  }
+});
+
+export default {
+  install (Vue) {
+    Vue.component('Editor', Editor)
+  }
+}
